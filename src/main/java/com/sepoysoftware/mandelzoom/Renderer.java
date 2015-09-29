79a44e4180260880d433df7de4590c7e6efb5135
rajibsingh <rajibsingh@yahoo.com>
2015-09-28 22:53:19 -0400

    trying to write test image

--- src/main/java/com/sepoysoftware/mandelzoom/Renderer.java
+++ src/main/java/com/sepoysoftware/mandelzoom/Renderer.java
@@ -1,6 +1,8 @@
 package com.sepoysoftware.mandelzoom;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelFormat;
+import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 
 /**
@@ -17,6 +19,12 @@ public class Renderer {
 
     Image getImage() {
         WritableImage image = new WritableImage(width, height);
+        PixelWriter writer = image.getPixelWriter();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+               writer.setArgb(x, y, 0000);
+            }
+        }
         return image;
     }
 }
